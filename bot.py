30d83dd321c0293ae4a298900c134a7d46c8d829
KartikM83 <117511537+KartikM83@users.noreply.github.com>
2023-01-03 22:18:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -36,72 +36,71 @@ LOGGER = logging.getLogger(__name__)
 TIMEZONE = (os.environ.get("TIMEZONE", "Asia/Kolkata"))
 
 # Bot Client for Inline Search
-Bot = Client(
+class Bot(Client):
 	def __init__(self):
-	super().__init__(
-		name=SESSION,
-		api_id=API_ID,
-		api_hash=API_HASH,
-		bot_token=BOT_TOKEN,
-		workers=50,
-		plugins={"root": "CYNITE"},
-		sleep_threshold=5,
+		super().__init__(
+			name=SESSION,
+			api_id=API_ID,
+			api_hash=API_HASH,
+			bot_token=BOT_TOKEN,
+			workers=50,
+			plugins={"root": "CYNITE"},
+			sleep_threshold=5,
         )
-)
+
 
 # User Client for Searching in Channe
-@Bot.on_message(filters.private & filters.command("start"))
-async def start_handler(_, event: Message):
-	await event.reply_photo("https://telegra.ph/file/165941ae764a56d6d9c89.jpg",
-                                caption=START_MSG.format(event.from_user.mention),
-                                reply_markup=InlineKeyboardMarkup([
-                                    [InlineKeyboardButton("Our Channel", url="https://t.me/cynitemovies"),
-                                     InlineKeyboardButton("Creator", url="https://t.me/Cyniteofficial")],
-                                    [InlineKeyboardButton("Help", callback_data="Help_msg"),
-                                     InlineKeyboardButton("About", callback_data="About_msg")]]))
+	@Bot.on_message(filters.private & filters.command("start"))
+	async def start_handler(_, event: Message):
+		await event.reply_photo("https://telegra.ph/file/165941ae764a56d6d9c89.jpg",
+                                	caption=START_MSG.format(event.from_user.mention),
+                                	reply_markup=InlineKeyboardMarkup([
+                                    	[InlineKeyboardButton("Our Channel", url="https://t.me/cynitemovies"),
+                                    	 InlineKeyboardButton("Creator", url="https://t.me/Cyniteofficial")],
+                                    	[InlineKeyboardButton("Help", callback_data="Help_msg"),
+                                     	InlineKeyboardButton("About", callback_data="About_msg")]]))
 
-@Bot.on_message(filters.private & filters.command("help"))
-async def help_handler(_, event: Message):
-
-    await event.reply_text(ABOUT_HELP_TEXT.format(event.from_user.mention),
-        reply_markup=InlineKeyboardMarkup([
-            [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
-             InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup"), 
-             InlineKeyboardButton("About", callback_data="About_msg")]
+	@Bot.on_message(filters.private & filters.command("help"))
+	async def help_handler(_, event: Message):
+		await event.reply_text(ABOUT_HELP_TEXT.format(event.from_user.mention),
+			       	reply_markup=InlineKeyboardMarkup([
+            	[InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
+             	InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup"), 
+             	InlineKeyboardButton("About", callback_data="About_msg")]
         ])
     )
 
-@Bot.on_message(filters.incoming)
-async def inline_handlers(_, event: Message):
-    if event.text == '/start':
-        return
-    answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
-    async for message in User.search_messages(chat_id=CHANNEL_ID, limit=50, query=event.text):
-        if message.text:
-            thumb = None
-            f_text = message.text
-            msg_text = message.text.html
-            if "|||" in message.text:
-                f_text = message.text.split("|||", 1)[0]
-                msg_text = message.text.html.split("|||", 1)[0]
-            answers += f'**🍿 Title ➠ ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n📜 About ➠ ' + '' + f_text.split("\n", 2)[-1] + ' \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\nLink Will Auto Delete In 60Sec...⏰\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
-    try:
-        msg = await event.reply_text(answers)
-        await asyncio.sleep(300)
-        await event.delete()
-        await msg.delete()
-    except:
-        print(f"[{BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
+	@Bot.on_message(filters.incoming)
+	async def inline_handlers(_, event: Message):
+		if event.text == '/start':
+			return
+		answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+		async for message in User.search_messages(chat_id=CHANNEL_ID, limit=50, query=event.text):
+			if message.text:
+				thumb = None
+				f_text = message.text
+				msg_text = message.text.html
+				if "|||" in message.text:
+					f_text = message.text.split("|||", 1)[0]
+					msg_text = message.text.html.split("|||", 1)[0]
+					answers += f'**🍿 Title ➠ ' + '' + f_text.split("\n", 1)[0] + '' + '\n\n📜 About ➠ ' + '' + f_text.split("\n", 2)[-1] + ' \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\nLink Will Auto Delete In 60Sec...⏰\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
+					try:
+						msg = await event.reply_text(answers)
+						await asyncio.sleep(300)
+						await event.delete()
+						await msg.delete()
+					except:
+						print(f"[{BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
 
 
-@Bot.on_callback_query()
-async def button(bot, cmd: CallbackQuery):
-        cb_data = cmd.data
-        if "About_msg" in cb_data:
-            await cmd.message.edit(
-			text=ABOUT_BOT_TEXT,
-			disable_web_page_preview=True,
-			reply_markup=InlineKeyboardMarkup(
+	@Bot.on_callback_query()
+	async def button(bot, cmd: CallbackQuery):
+        	cb_data = cmd.data
+        	if "About_msg" in cb_data:
+            		await cmd.message.edit(
+				text=ABOUT_BOT_TEXT,
+				disable_web_page_preview=True,
+				reply_markup=InlineKeyboardMarkup(
 				[
 					[
 						InlineKeyboardButton("Our Channel", url="https://t.me/cyniteMovies"),
@@ -113,13 +112,13 @@ async def button(bot, cmd: CallbackQuery):
 					]
 				]
 			),
-			parse_mode="html"
+				parse_mode="html"
 		)
-        elif "Help_msg" in cb_data:
-            await cmd.message.edit(
-			text=ABOUT_HELP_TEXT,
-			disable_web_page_preview=True,
-			reply_markup=InlineKeyboardMarkup(
+		elif "Help_msg" in cb_data:
+            		await cmd.message.edit(
+				text=ABOUT_HELP_TEXT,
+				disable_web_page_preview=True,
+				reply_markup=InlineKeyboardMarkup(
 				[
 					[
 						InlineKeyboardButton("About", callback_data="About_msg"),
@@ -133,11 +132,11 @@ async def button(bot, cmd: CallbackQuery):
 			),
 			parse_mode="html"
 		)
-        elif "gohome" in cb_data:
-            await cmd.message.edit(
-			text=START_MSG.format(cmd.from_user.mention),
-			disable_web_page_preview=True,
-			reply_markup=InlineKeyboardMarkup(
+		elif "gohome" in cb_data:
+            	await cmd.message.edit(
+				text=START_MSG.format(cmd.from_user.mention),
+				disable_web_page_preview=True,
+				reply_markup=InlineKeyboardMarkup(
 				[
                                         [
 						InlineKeyboardButton("Help", callback_data="Help_msg"),
@@ -149,7 +148,7 @@ async def button(bot, cmd: CallbackQuery):
 					]
 				]
 			),
-			parse_mode="html"
+				parse_mode="html"
 		)
 
 # Start Clients
