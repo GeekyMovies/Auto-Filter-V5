301a5de2b1088d1fdf431b2157cffe1c8bc7b15c
KartikM83 <117511537+KartikM83@users.noreply.github.com>
2023-01-03 21:24:01 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -232,14 +232,6 @@ class Bot(Client):
         escape_url = urllib.parse.quote(str)
         return escape_url
 
-# Bot Client for Inline Search
-    Bot = Client(
-        session_name=BOT_SESSION_NAME,
-        api_id=API_ID,
-        api_hash=API_HASH,
-        bot_token=BOT_TOKEN,
-        plugins=dict(root="plugins")
-    )
                 
                 
 app = Bot()
