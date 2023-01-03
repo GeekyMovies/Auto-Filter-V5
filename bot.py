274e6876183b24ee16349ad637a282a482a04137
KartikM83 <117511537+KartikM83@users.noreply.github.com>
2023-01-03 21:51:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,4 +1,4 @@
-from configs import Config
+from info import SESSION, API_ID, API_HASH, BOT_TOKEN, LOG_STR, LOG_CHANNEL, PORT, CHANNELS, UPDATES_CHANNEL_USERNAME,BOT_USERNAME, BOT_SESSION_NAME, START_MSG, ABOUT_HELP_TEXT
 from pyrogram import Client, filters, idle
 from pyrogram.errors import QueryIdInvalid
 from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, CallbackQuery, InlineQuery, InlineQueryResultArticle, \
@@ -8,23 +8,23 @@ import asyncio
 
 # Bot Client for Inline Search
 Bot = Client(
-    session_name=Config.BOT_SESSION_NAME,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH,
-    bot_token=Config.BOT_TOKEN
+    session_name=BOT_SESSION_NAME,
+    api_id=API_ID,
+    api_hash=API_HASH,
+    bot_token=BOT_TOKEN
 )
 
 # User Client for Searching in Channel.
 User = Client(
-    session_name=Config.USER_SESSION_STRING,
-    api_id=Config.API_ID,
-    api_hash=Config.API_HASH
+    session_name=USER_SESSION_STRING,
+    api_id=API_ID,
+    api_hash=API_HASH
 )
 
 @Bot.on_message(filters.private & filters.command("start"))
 async def start_handler(_, event: Message):
 	await event.reply_photo("https://telegra.ph/file/165941ae764a56d6d9c89.jpg",
-                                caption=Config.START_MSG.format(event.from_user.mention),
+                                caption=START_MSG.format(event.from_user.mention),
                                 reply_markup=InlineKeyboardMarkup([
                                     [InlineKeyboardButton("Our Channel", url="https://t.me/cynitemovies"),
                                      InlineKeyboardButton("Creator", url="https://t.me/Cyniteofficial")],
@@ -34,7 +34,7 @@ async def start_handler(_, event: Message):
 @Bot.on_message(filters.private & filters.command("help"))
 async def help_handler(_, event: Message):
 
-    await event.reply_text(Config.ABOUT_HELP_TEXT.format(event.from_user.mention),
+    await event.reply_text(ABOUT_HELP_TEXT.format(event.from_user.mention),
         reply_markup=InlineKeyboardMarkup([
             [InlineKeyboardButton("Our Channel", url="https://t.me/iP_Movies"),
              InlineKeyboardButton("Our Group", url="https://t.me/iPopcornMovieGroup"), 
@@ -47,7 +47,7 @@ async def inline_handlers(_, event: Message):
     if event.text == '/start':
         return
     answers = f'**📂 Results For ➠ {event.text} \n\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n➠ Type Only Movie Name With Correct Spelling.✍️\n➠ Add Year For Better Result.🗓️\n▰▱▰▱▰▱▰▱▰▱▰▱▰▱\n\n**'
-    async for message in User.search_messages(chat_id=Config.CHANNEL_ID, limit=50, query=event.text):
+    async for message in User.search_messages(chat_id=CHANNEL_ID, limit=50, query=event.text):
         if message.text:
             thumb = None
             f_text = message.text
@@ -62,7 +62,7 @@ async def inline_handlers(_, event: Message):
         await event.delete()
         await msg.delete()
     except:
-        print(f"[{Config.BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
+        print(f"[{BOT_SESSION_NAME}] - Failed to Answer - {event.from_user.first_name}")
 
 
 @Bot.on_callback_query()
@@ -70,7 +70,7 @@ async def button(bot, cmd: CallbackQuery):
         cb_data = cmd.data
         if "About_msg" in cb_data:
             await cmd.message.edit(
-			text=Config.ABOUT_BOT_TEXT,
+			text=ABOUT_BOT_TEXT,
 			disable_web_page_preview=True,
 			reply_markup=InlineKeyboardMarkup(
 				[
@@ -88,7 +88,7 @@ async def button(bot, cmd: CallbackQuery):
 		)
         elif "Help_msg" in cb_data:
             await cmd.message.edit(
-			text=Config.ABOUT_HELP_TEXT,
+			text=ABOUT_HELP_TEXT,
 			disable_web_page_preview=True,
 			reply_markup=InlineKeyboardMarkup(
 				[
@@ -106,7 +106,7 @@ async def button(bot, cmd: CallbackQuery):
 		)
         elif "gohome" in cb_data:
             await cmd.message.edit(
-			text=Config.START_MSG.format(cmd.from_user.mention),
+			text=START_MSG.format(cmd.from_user.mention),
 			disable_web_page_preview=True,
 			reply_markup=InlineKeyboardMarkup(
 				[
