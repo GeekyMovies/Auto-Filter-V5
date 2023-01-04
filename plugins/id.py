4d12e9aa6ef9e85c21b83a1ae52773f6ea6ba445
KartikM83 <117511537+KartikM83@users.noreply.github.com>
2023-01-04 21:26:34 +0530

    Delete plugins directory

--- plugins/id.py
+++
@@ -1,6 +0,0 @@
-from pyrogram import Client, filters
-
-
-@Client.on_message(filters.text & filters.incoming & filters.command("id"))
-async def id(_, msg):
-    await msg.reply(f" Here Is ID : `{msg.chat.id}`", quote=True)
