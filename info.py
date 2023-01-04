882e066ef6e7fb845d11eebfa9c0a4eb6ee4f5b1
KartikM83 <117511537+KartikM83@users.noreply.github.com>
2023-01-04 21:38:27 +0530

    Update info.py

--- info.py
+++ info.py
@@ -19,15 +19,8 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "5823060125:AAHQGGkl7MGCJcz42n0taJBik2o9W45
 BOT_USERNAME = environ.get("BOT_USERNAME", "geekyofficialbot")
 BOT_SESSION_NAME = environ.get("BOT_SESSION_NAME", "geekybot")
 UPDATES_CHANNEL = environ.get("UPDATES_CHANNEL", "-1001249072794")
-
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1001796458234"))
-RESULTS_COUNT = int(environ.get("RESULTS_COUNT", 20))
-BROADCAST_AS_COPY = environ.get("BROADCAST_AS_COPY", "True")
-
-FORCE_SUB = environ.get("FORCE_SUB", "False")
-
 MDISK_API = environ.get("MDISK_API", "Qu7jX9V0Sn3q1JHdxjPp")
-VERIFIED_TIME  = int(environ.get("VERIFIED_TIME", "31"))
+
 
 
 ABOUT_BOT_TEXT = """<b>This is Mdisk Search Bot.
